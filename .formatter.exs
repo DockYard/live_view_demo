6a867f2163dd69ecce94a334e9c4a18232ba3d7c
Jonathan Principe <jprincipe@jp-macbook-pro.lan>
2019-08-28 22:04:17 -0400

    setup line length to 280

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
   import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 280
 ]
