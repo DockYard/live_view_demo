3cdd42a4eef8b4c7db38bb42bc5a2db0928e6253
Jon Principe <jon@rocketinsights.com>
2019-08-29 14:41:30 -0400

    Add Universe History (#2)

--- lib/game_of_life/cell.ex
+++ lib/game_of_life/cell.ex
@@ -12,13 +12,15 @@ defmodule GameOfLife.Cell do
     )
   end
 
-  def tick(universe_name, position), do: GenServer.call(via_tuple(universe_name, position), :tick)
+  def tick(universe_name, position, generation), do: GenServer.call(via_tuple(universe_name, position), {:tick, generation})
 
   def crash(universe_name, position), do: GenServer.cast(via_tuple(universe_name, position), :crash)
 
-  def alive?(universe_name, position) do
+  def info(universe_name, position, generation), do: GenServer.call(via_tuple(universe_name, position), {:info, generation})
+
+  def alive?(universe_name, position, generation) do
     if exists?(universe_name, position) do
-      GenServer.call(via_tuple(universe_name, position), :alive)
+      GenServer.call(via_tuple(universe_name, position), {:alive, generation})
     else
       nil
     end
@@ -28,48 +30,53 @@ defmodule GameOfLife.Cell do
 
   @impl true
   def init(state) do
-    alive = Enum.random([true, false])
-    state = Map.put(state, :alive, alive)
+    alive = Map.get(state, :active, Enum.random([true, false]))
+    state = Map.put(state, :history, [alive])
 
     {:ok, state}
   end
 
   @impl true
-  def handle_call(:tick, _from, state) do
-    state = update_cell(state)
+  def handle_call({:tick, generation}, _from, %{universe_name: universe_name, position: position, history: history} = state) do
+    alive = cell_state(state, generation - 1)
+    history = history ++ [alive]
 
-    {:reply, Map.get(state, :alive, false), state}
+    {:reply, %{universe_name: universe_name, position: position, alive: alive}, Map.put(state, :history, history)}
   end
 
   @impl true
-  def handle_call(:alive, _from, state) do
-    {:reply, Map.get(state, :alive), state}
+  def handle_call({:info, generation}, _from, %{universe_name: universe_name, position: position, history: history} = state) do
+    alive = Enum.at(history, generation)
+
+    {:reply, %{universe_name: universe_name, position: position, alive: alive}, state}
   end
 
   @impl true
-  def handle_cast(:crash, _state) do
-    raise "crashed"
+  def handle_call({:alive, generation}, _from, %{history: history} = state) do
+    {:reply, Enum.at(history, generation), state}
   end
+
+  @impl true
+  def handle_cast(:crash, _state), do: raise("💥crash💥")
 
   ## Utils
 
-  defp update_cell(state) do
-    alive_count = state |> neighbor_states() |> Enum.count(& &1)
-    alive = Enum.member?([2, 3], alive_count)
+  defp cell_state(state, generation) do
+    alive_count = state |> neighbor_states(generation) |> Enum.count(& &1)
 
-    Map.put(state, :alive, alive)
+    Enum.member?([2, 3], alive_count)
   end
 
-  defp neighbor_states(%{universe_name: universe_name, position: {x, y}}) do
+  defp neighbor_states(%{universe_name: universe_name, position: {x, y}}, generation) do
     [
-      alive?(universe_name, {x - 1, y}),
-      alive?(universe_name, {x - 1, y - 1}),
-      alive?(universe_name, {x, y - 1}),
-      alive?(universe_name, {x + 1, y - 1}),
-      alive?(universe_name, {x + 1, y}),
-      alive?(universe_name, {x + 1, y + 1}),
-      alive?(universe_name, {x, y + 1}),
-      alive?(universe_name, {x + 1, y})
+      alive?(universe_name, {x - 1, y}, generation),
+      alive?(universe_name, {x - 1, y - 1}, generation),
+      alive?(universe_name, {x, y - 1}, generation),
+      alive?(universe_name, {x + 1, y - 1}, generation),
+      alive?(universe_name, {x + 1, y}, generation),
+      alive?(universe_name, {x + 1, y + 1}, generation),
+      alive?(universe_name, {x, y + 1}, generation),
+      alive?(universe_name, {x + 1, y}, generation)
     ]
   end
 
--- lib/game_of_life/universe.ex
+++ lib/game_of_life/universe.ex
@@ -4,8 +4,8 @@ defmodule GameOfLife.Universe do
 
   @moduledoc """
   GameOfLife.Universe.Supervisor.start_child("u1", {5, 5})
-  GameOfLife.Universe.Supervisor.start_child("u2", {10, 10})
   GameOfLife.Universe.tick("u1")
+  GameOfLife.Universe.info("u1", 0)
   """
 
   alias GameOfLife.Cell
@@ -15,7 +15,7 @@ defmodule GameOfLife.Universe do
   def start_link(%{name: name, dimensions: dimensions}) do
     GenServer.start_link(
       __MODULE__,
-      %{name: name, dimensions: dimensions},
+      %{name: name, dimensions: dimensions, generation: 0},
       name: via_tuple(name)
     )
   end
@@ -26,38 +26,62 @@ defmodule GameOfLife.Universe do
 
   def tick(name), do: GenServer.call(via_tuple(name), :tick)
 
+  def info(name, generation), do: GenServer.call(via_tuple(name), {:info, generation})
+  def info(name), do: GenServer.call(via_tuple(name), :info)
+
   ## Server
 
   @impl true
   def init(%{name: name} = state) do
     GameOfLife.Cell.Supervisor.start_link(name)
 
-    each_cell(state, &GameOfLife.Cell.Supervisor.start_child/2)
-    print_universe(state)
+    initialize_cells(state)
 
     {:ok, state}
   end
 
   @impl true
-  def handle_call(:tick, _from, state) do
-    each_cell(state, &Cell.tick/2)
+  def handle_call(:tick, _from, %{generation: generation} = state) do
+    state = Map.put(state, :generation, generation + 1)
+    cells = each_cell(state, &Cell.tick/3)
+    print_universe(cells)
 
-    print_universe(state)
-
-    {:reply, state, state}
+    {:reply, cells, state}
   end
 
   @impl true
-  def handle_cast(:crash, _state) do
-    raise "imploding"
+  def handle_call(:info, _from, state) do
+    cells = each_cell(state, &Cell.info/3)
+    print_universe(cells)
+
+    {:reply, cells, state}
   end
+
+  @impl true
+  def handle_call({:info, generation}, _from, state) do
+    cells = each_cell(Map.put(state, :generation, generation), &Cell.info/3)
+    print_universe(cells)
+
+    {:reply, cells, state}
+  end
+
+  @impl true
+  def handle_cast(:crash, _state), do: raise("💥kaboom💥")
 
   ## Utils
 
-  defp print_universe(%{name: name, dimensions: {height, width}}) do
-    Enum.each(0..height, fn y ->
-      Enum.each(0..width, fn x ->
-        case Cell.alive?(name, {x, y}) do
+  defp initialize_cells(%{name: name, dimensions: {height, width}}) do
+    Enum.map(0..height, fn y ->
+      Enum.map(0..width, fn x ->
+        GameOfLife.Cell.Supervisor.start_child(name, {x, y})
+      end)
+    end)
+  end
+
+  defp print_universe(cells) do
+    Enum.each(cells, fn row ->
+      Enum.each(row, fn %{alive: alive} ->
+        case alive do
           nil -> "-"
           false -> "X"
           true -> "0"
@@ -69,10 +93,10 @@ defmodule GameOfLife.Universe do
     end)
   end
 
-  defp each_cell(%{name: name, dimensions: {height, width}}, f) do
-    Enum.each(0..height, fn y ->
-      Enum.each(0..width, fn x ->
-        f.(name, {x, y})
+  defp each_cell(%{name: name, dimensions: {height, width}, generation: generation}, f) do
+    Enum.map(0..height, fn y ->
+      Enum.map(0..width, fn x ->
+        f.(name, {x, y}, generation)
       end)
     end)
   end
