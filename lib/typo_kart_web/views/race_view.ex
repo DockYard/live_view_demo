ccbff6f57611d139aada0a0ae64ae69276ddf628
Mike Wilkerson <mwilkerson@gmail.com>
2019-09-16 06:32:05 -0700

    simplify map_transform arguments

--- lib/typo_kart_web/views/race_view.ex
+++ lib/typo_kart_web/views/race_view.ex
@@ -1,8 +1,11 @@
 defmodule TypoKartWeb.RaceView do
   use TypoKartWeb, :view
 
-  def map_transform(_cur_char_num, cur_char_rotation, _map_angle) do
-    "translate(250,250) rotate(#{map_angle(cur_char_rotation)}, 250, 250)"
+  @base_translate_x 250
+  @base_translate_y 250
+
+  def map_transform(cur_char_rotation) do
+    "translate(#{@base_translate_x},#{@base_translate_y}) rotate(#{map_angle(cur_char_rotation)}, #{@base_translate_x}, #{@base_translate_y})"
   end
 
   def map_angle(cur_char_rotation), do: -1 * cur_char_rotation
