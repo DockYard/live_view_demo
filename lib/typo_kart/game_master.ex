ab007720f5840ec31931f491a74b72cfa53af3d4
Mike Wilkerson <mwilkerson@gmail.com>
2019-09-18 06:24:34 -0700

    add new_game and test

--- lib/typo_kart/game_master.ex
+++ lib/typo_kart/game_master.ex
@@ -1,6 +1,8 @@
 defmodule TypoKart.GameMaster do
   use GenServer
 
+  alias TypoKart.Game
+
   def start_link(_init \\ nil) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
@@ -15,8 +17,17 @@ defmodule TypoKart.GameMaster do
     {:reply, state, state}
   end
 
+  def handle_call({:new_game, game}, _from, state) do
+    id = UUID.uuid1()
+    {:reply, id, put_in(state, [:games, id], game)}
+  end
+
   def state do
     GenServer.call(__MODULE__, :state)
+  end
+
+  def new_game(%Game{} = game \\ %Game{}) do
+    GenServer.call(__MODULE__, {:new_game, game})
   end
 
   #def handle_cast(:new_game, _from, state) do
--- test/typo_kart/game_master_test.exs
+++ test/typo_kart/game_master_test.exs
@@ -1,9 +1,22 @@
 defmodule TypoKart.GameMasterTest do
   use TypoKart.PlainCase
 
-  alias TypoKart.GameMaster
+  alias TypoKart.{
+    Game,
+    GameMaster
+  }
 
   test "initializes" do
     assert %{games: %{}} = GameMaster.state()
+  end
+
+  test "creates new default game" do
+    assert id = GameMaster.new_game()
+
+    assert %{
+      games: %{
+        ^id => %Game{}
+      }
+    } = GameMaster.state()
   end
 end
