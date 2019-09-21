00f4a23ccc1b684e5aac7f24a517a3207ac85427
Mike Wilkerson <mwilkerson@gmail.com>
2019-09-21 08:38:33 -0700

    Add path_braches to Course struct

--- lib/typo_kart/course.ex
+++ lib/typo_kart/course.ex
@@ -1,5 +1,8 @@
 defmodule TypoKart.Course do
-  alias TypoKart.Path
+  alias TypoKart.{
+    Path,
+    PathCharIndex
+  }
 
   defstruct initial_rotation: 0,
     base_translate_x: 0,
@@ -7,7 +10,8 @@ defmodule TypoKart.Course do
     marker_center_offset_x: 0,
     marker_center_offset_y: 0,
     view_box: "",
-    paths: []
+    paths: [],
+    path_branches: []
 
   @type t :: %__MODULE__ {
     initial_rotation: integer(),
@@ -16,6 +20,7 @@ defmodule TypoKart.Course do
     view_box: binary(),
     marker_center_offset_x: integer(),
     marker_center_offset_y: integer(),
-    paths: list(Path.t())
+    paths: list(Path.t()),
+    path_branches: list(PathCharIndex.t())
   }
 end
