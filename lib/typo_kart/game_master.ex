6c66e90b6816806247c96987da9e5d4f27ccd409
Mike Wilkerson <mwilkerson@gmail.com>
2019-09-21 14:42:41 -0700

    Implement and test next_chars/2

--- lib/typo_kart/game_master.ex
+++ lib/typo_kart/game_master.ex
@@ -5,7 +5,8 @@ defmodule TypoKart.GameMaster do
     Game,
     Course,
     Path,
-    PathCharIndex
+    PathCharIndex,
+    Player
   }
 
   def start_link(_init \\ nil) do
@@ -18,6 +19,12 @@ defmodule TypoKart.GameMaster do
     }}
   end
 
+  def handle_call(:reset_all, _from, state) do
+    {:ok, reset_state} = init()
+
+    {:reply, :ok, reset_state}
+  end
+
   def handle_call(:state, _from, state) do
     {:reply, state, state}
   end
@@ -28,47 +35,32 @@ defmodule TypoKart.GameMaster do
   end
 
   def handle_call({:advance_game, game_id, player_index, key_code}, _from, state) do
-    {:reply, %Game{}, state}
+    # If key_code fits one of the characters (we'll take the first one found) indexed by the player's
+    # cur_path_char_indices, then we can advance.
+    with %Game{course: course} = game <- Kernel.get_in(state, [:games, game_id]),
+      %Player{cur_path_char_indices: cur_path_char_indices} = player <- Enum.at(game.players, player_index),
+      %PathCharIndex{} = valid_index <- Enum.find(cur_path_char_indices, &(char_from_course(course, &1) == key_code))
+    do
+      {:reply, {:ok, game}, state} # temporary FAKE result
 
-    # %Player{cur_path_chars: cur_path_chars} = Enum.at(game.players, player)
+      # Update game state to reflect this player's progress
 
-    # Enum.reduce(cur_path_chars, nil, fn (%PathChar{path: path, char: char}, acc) ->
-    #   # does this one match the current key?
-    #   case String.slice(Enum.at(course_paths, path).text, char..char) do
-    #     x when key == x or (key == "_" and x == " ") ->
-    #       Logger.debug("GOOD key: advance")
-    #       # Mutate the game.
-    #       # For the current player who has just keyed something correctly,
-    #       # mark the current text slice as his, and then recompute the next
-    #       {:ok, game}
+      # 1. Discover all of the possible path_char_indices that are now valid for this player.
+      #    Set those as the new possible chars for that player.
+      #
+      # 2. Mark this point on the course as claimed by this player.
+      #
+      # 3. Accumulate any points that may be relevant 
+    else
+      _bad ->
+        IO.puts("BAD: #{inspect(_bad)}")
+        {:reply, {:error, "bad key_code"}, state}
+    end
+  end
 
-    #     bad ->
-    #       Logger.debug("BAD Key: player=#{player}, key=#{key}, cur_path=#{cur_path}, cur_char=#{cur_char}, cur_text=\"#{bad}\"")
-    #       :error
-    #   end
-
-    # end)
-    # # Find the first matching cur_path _char
-    # with ,
-    #   %PathChar{path: cur_path, char: cur_char} <- Enum.find(cur_path_chars, &(&1))
-    #   %{text: text} <- Enum.at(map.paths, cur_path) do
-    #   case String.slice(text, cur_char..cur_char) do
-    #     x when key == x or (key == "_" and x == " ") ->
-    #       Logger.debug("GOOD key: advance")
-    #       # Mutate the game.
-    #       # For the current player who has just keyed something correctly,
-    #       # mark the current text slice as his, and then recompute the next
-    #       {:ok, game}
-
-    #     bad ->
-    #       Logger.debug("BAD Key: player=#{player}, key=#{key}, cur_path=#{cur_path}, cur_char=#{cur_char}, cur_text=\"#{bad}\"")
-    #       :error
-    #   end
-    # else
-    #   bad ->
-    #     Logger.debug("ERROR: #{inspect(bad)}")
-    #     :error
-    # end
+  @spec reset_all() :: :ok
+  def reset_all do
+    GenServer.call(__MODULE__, :reset_all)
   end
 
   @spec state() :: map()
@@ -96,5 +88,33 @@ defmodule TypoKart.GameMaster do
   def advance(game_id, player_index, key_code)
     when is_binary(game_id) and is_integer(player_index) and is_integer(key_code) do
     GenServer.call(__MODULE__, {:advance_game, game_id, player_index, key_code})
+  end
+
+  @spec next_chars(Course.t(), PathCharIndex.t()) :: list(PathCharIndex.t())
+  def next_chars(%Course{paths: paths, path_branches: path_branches}, %PathCharIndex{path_index: cur_path_index, char_index: cur_char_index} = path_char_index) do
+    %Path{chars: cur_path_chars} = Enum.at(paths, cur_path_index)
+
+    # 1. Add the next char_index on the current path. It's always a valid next char.
+    #    If we're at the end of the path, wrap back around to index 0 on that path.
+    cur_path_next_char_index =
+      case %PathCharIndex{path_index: cur_path_index, char_index: cur_char_index + 1} do
+        %PathCharIndex{char_index: next_char_index} = pci
+          when next_char_index < length(cur_path_chars) ->
+            pci
+        
+        _ ->
+          %PathCharIndex{path_index: cur_path_index, char_index: 0}
+      end
+
+    # 2. If the next char on the current path is a branch point onto another path,
+    #    then add the index on that other path.
+    [cur_path_next_char_index] ++
+      Enum.reduce(path_branches, [], fn
+        {%PathCharIndex{} = pci_from, %PathCharIndex{} = pci_to}, acc when pci_from == cur_path_next_char_index ->
+          acc ++ [pci_to]
+
+        _, acc ->
+          acc
+      end)
   end
 end
--- test/typo_kart/game_master_test.exs
+++ test/typo_kart/game_master_test.exs
@@ -10,6 +10,10 @@ defmodule TypoKart.GameMasterTest do
     PathCharIndex
   }
 
+  setup do
+    GameMaster.reset_all()
+  end
+
   test "initializes" do
     assert %{games: %{}} = GameMaster.state()
   end
@@ -22,6 +26,22 @@ defmodule TypoKart.GameMasterTest do
         ^id => %Game{}
       }
     } = GameMaster.state()
+  end
+
+  test "reset_all re-initializes everything" do
+    assert id = GameMaster.new_game()
+
+    assert %{
+      games: %{
+        ^id => %Game{}
+      }
+    } = GameMaster.state()
+
+    assert :ok = GameMaster.reset_all()
+
+    {:ok, initial_state} = GameMaster.init()
+
+    assert initial_state == GameMaster.state()
   end
 
   test "creates a game with some initialization" do
@@ -88,6 +108,90 @@ defmodule TypoKart.GameMasterTest do
     path_char_index = %PathCharIndex{path_index: 0, char_index: 40}
 
     assert nil == GameMaster.char_from_course(course, path_char_index)
+  end
+
+  @tag :next_chars
+  test "next_chars/2 single path, with no branches" do
+    course = %Course{
+      paths: [
+        %Path{
+          chars: String.to_charlist("The quick brown fox")
+        }
+      ],
+      path_branches: []
+    }
+
+    assert [
+      %PathCharIndex{path_index: 0, char_index: 4}
+    ] = GameMaster.next_chars(course, %PathCharIndex{path_index: 0, char_index: 3})
+  end
+
+  @tag :next_chars
+  test "next_chars/2 wrap around on the current path" do
+    course = %Course{
+      paths: [
+        %Path{
+          chars: String.to_charlist("fox")
+        }
+      ],
+      path_branches: []
+    }
+
+    assert [
+      %PathCharIndex{path_index: 0, char_index: 0}
+    ] = GameMaster.next_chars(course, %PathCharIndex{path_index: 0, char_index: 2})
+  end
+
+  @tag :next_chars
+  test "next_chars/2 with a branch, taking the branch path" do
+    course = %Course{
+      paths: [
+        %Path{
+          chars: String.to_charlist("The quick brown fox")
+        },
+        %Path{
+          chars: String.to_charlist("A slow green turtle")
+        }
+      ],
+      path_branches: [
+        {
+          # A player can advance directly from this point...
+          %PathCharIndex{path_index: 0, char_index: 9},
+          # ...to this point.
+          %PathCharIndex{path_index: 1, char_index: 0}
+        }
+      ]
+    }
+
+    assert [
+      %PathCharIndex{path_index: 1, char_index: 1}
+    ] = GameMaster.next_chars(course, %PathCharIndex{path_index: 1, char_index: 0})
+  end
+
+  @tag :next_chars
+  test "next_chars/2 with a branch, remaining on the current path" do
+    course = %Course{
+      paths: [
+        %Path{
+          chars: String.to_charlist("The quick brown fox")
+        },
+        %Path{
+          chars: String.to_charlist("A slow green turtle")
+        }
+      ],
+      path_branches: [
+        {
+          # A player can advance directly from this point...
+          %PathCharIndex{char_index: 9, path_index: 0},
+          # ...to this point.
+          %PathCharIndex{char_index: 0, path_index: 1}
+        }
+      ]
+    }
+
+    assert [
+      %PathCharIndex{char_index: 10, path_index: 0}
+    ] = GameMaster.next_chars(course, %PathCharIndex{char_index: 9, path_index: 0})
   end
 
   test "advance/3 with valid single path inputs and single current path_char index for given player" do
