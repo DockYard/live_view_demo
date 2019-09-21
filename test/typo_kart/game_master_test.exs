43b80adbfc084442d403a4283571ca593a900269
Mike Wilkerson <mwilkerson@gmail.com>
2019-09-21 07:55:29 -0700

    test various advance/4 scenarios

--- test/typo_kart/game_master_test.exs
+++ test/typo_kart/game_master_test.exs
@@ -83,4 +83,180 @@ defmodule TypoKart.GameMasterTest do
 
     assert nil == GameMaster.char_from_course(course, path_char_index)
   end
+
+  test "advance/4 with valid single path inputs and single current path_char index for given player" do
+    course = %Course{paths: [
+      %Path{
+        chars: String.to_charlist("The quick brown fox")
+      }
+    ]}
+
+    game = %Game{
+      players: [
+        %Player{
+          cur_path_char_indices: [
+            %PathCharIndex{
+              path_index: 0,
+              char_index: 4
+            }
+          ]
+        }
+      ]
+    }
+
+    assert {:ok, game} = GameMaster.advance(course, game, 0, hd('q'))
+
+    assert %Game{
+      players: %Player{
+        cur_path_char_indices: [
+          %PathCharIndex{
+            path_index: 0,
+            char_index: 5 # incremented
+          }
+        ]
+      }
+    } = game
+  end
+
+  test "advance/4 following a path branch" do
+    course = %Course{
+      paths: [
+        %Path{
+          chars: String.to_charlist("The quick brown fox")
+        },
+        %Path{
+          chars: String.to_charlist("A slow green turtle")
+        }
+      ],
+      path_branches: [
+        {
+          # A player can advance directly from this point...
+          %PathCharIndex{path_index: 0, char_index: 9},
+          # ...to this point.
+          %PathCharIndex{path_index: 1, char_index: 0}
+        }
+      ]
+    }
+
+    game = %Game{
+      players: [
+        %Player{
+          cur_path_char_indices: [
+            %PathCharIndex{
+              path_index: 0,
+              char_index: 10
+            },
+            %PathCharIndex{
+              path_index: 1,
+              char_index: 0
+            }
+          ]
+        }
+      ]
+    }
+
+    assert {:ok, game} = GameMaster.advance(course, game, 0, hd('A'))
+
+    assert %Game{
+      players: %Player{
+        cur_path_char_indices: [
+          %PathCharIndex{
+            path_index: 1,
+            char_index: 1 # moved onto new path
+          }
+        ]
+      }
+    } = game
+  end
+
+  test "advance/4 remaining on the same path passing a branch point" do
+    course = %Course{
+      paths: [
+        %Path{
+          chars: String.to_charlist("The quick brown fox")
+        },
+        %Path{
+          chars: String.to_charlist("A slow green turtle")
+        }
+      ],
+      path_branches: [
+        {
+          # A player can advance directly from this point...
+          %PathCharIndex{path_index: 0, char_index: 9},
+          # ...to this point.
+          %PathCharIndex{path_index: 1, char_index: 0}
+        }
+      ]
+    }
+
+    game = %Game{
+      players: [
+        %Player{
+          cur_path_char_indices: [
+            %PathCharIndex{
+              path_index: 0,
+              char_index: 10
+            },
+            %PathCharIndex{
+              path_index: 1,
+              char_index: 0
+            }
+          ]
+        }
+      ]
+    }
+
+    assert {:ok, game} = GameMaster.advance(course, game, 0, hd('A'))
+
+    assert %Game{
+      players: %Player{
+        cur_path_char_indices: [
+          %PathCharIndex{
+            path_index: 0,
+            char_index: 11 # advanced along the same path
+          }
+        ]
+      }
+    } = game
+  end
+
+  test "advance/4 invalid keyCode at a path branch" do
+    course = %Course{
+      paths: [
+        %Path{
+          chars: String.to_charlist("The quick brown fox")
+        },
+        %Path{
+          chars: String.to_charlist("A slow green turtle")
+        }
+      ],
+      path_branches: [
+        {
+          # A player can advance directly from this point...
+          %PathCharIndex{path_index: 0, char_index: 9},
+          # ...to this point.
+          %PathCharIndex{path_index: 1, char_index: 0}
+        }
+      ]
+    }
+
+    game = %Game{
+      players: [
+        %Player{
+          cur_path_char_indices: [
+            %PathCharIndex{
+              path_index: 0,
+              char_index: 10
+            },
+            %PathCharIndex{
+              path_index: 1,
+              char_index: 0
+            }
+          ]
+        }
+      ]
+    }
+
+    assert :error = GameMaster.advance(course, game, 0, hd('s'))
+  end
 end
