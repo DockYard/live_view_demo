2e49d4383b152810857ae7589e26969f7140da4b
Mike Wilkerson <mwilkerson@gmail.com>
2019-09-27 10:51:51 -0700

    re-work race_view helpers and add basic unit testing

--- lib/typo_kart_web/views/race_view.ex
+++ lib/typo_kart_web/views/race_view.ex
@@ -6,50 +6,63 @@ defmodule TypoKartWeb.RaceView do
     Game,
     GameMaster,
     Path,
+    PathCharIndex,
     Player
   }
 
-  def map_transform(
-        %Course{
-          base_translate_x: base_translate_x,
-          base_translate_y: base_translate_y,
-          course_rotation_center_x: course_rotation_center_x,
-          course_rotation_center_y: course_rotation_center_y
-        },
-        cur_char_rotation
-      ) do
-    "translate(#{base_translate_x},#{base_translate_y}) rotate(#{map_angle(cur_char_rotation)}, #{
+  @spec course_transform(Game.t(), list(ViewChar.t()), integer()) :: binary()
+  def course_transform(
+        %Game{
+          course: %Course{
+            base_translate_x: base_translate_x,
+            base_translate_y: base_translate_y,
+            course_rotation_center_x: course_rotation_center_x,
+            course_rotation_center_y: course_rotation_center_y
+          }
+        } = game,
+        view_chars,
+        player_index
+      ) when is_list(view_chars) and is_integer(player_index) do
+
+    "translate(#{base_translate_x},#{base_translate_y}) rotate(#{course_rotation(game, view_chars, player_index)}, #{
       course_rotation_center_x
     }, #{course_rotation_center_y})"
   end
 
+  @spec marker_transform(Game.t(), list(ViewChar.t()), integer(), float(), float(), float()) :: binary()
   def marker_transform(
-        %Course{
-          marker_center_offset_x: marker_center_offset_x,
-          marker_center_offset_y: marker_center_offset_y
-        },
-        cur_char_rotation,
-        [cur_char_x, cur_char_y],
+        %Game{
+          course: %Course{
+            marker_center_offset_x: marker_center_offset_x,
+            marker_center_offset_y: marker_center_offset_y
+          }
+        } = game,
+        view_chars,
+        player_index,
         marker_rotation_offset,
         marker_translate_offset_x,
         marker_translate_offset_y
-      ) do
-    "rotate(#{cur_char_rotation + marker_rotation_offset}, #{cur_char_x}, #{cur_char_y}) translate(#{
+      ) when is_list(view_chars) and is_integer(player_index) do
+        %{ x: cur_char_x, y: cur_char_y, rotation: cur_char_rotation } =
+          cur_view_char(game, view_chars, player_index)
+
+      "rotate(#{cur_char_rotation + marker_rotation_offset}, #{cur_char_x}, #{cur_char_y}) translate(#{
       cur_char_x - marker_center_offset_x + marker_translate_offset_x
     }, #{cur_char_y - marker_center_offset_y + marker_translate_offset_y}) scale(0.2)"
   end
 
-  def cur_text_path_id(%Game{} = game, player_index) when is_integer(player_index) do
-    "text-path-#{cur_path_char_index(game, player_index) |> Map.get(:path_index)}"
-  end
-
+  @spec cur_char_index(Game.t(), integer()) :: PathCharIndex.t()
   def cur_char_index(%Game{} = game, player_index) when is_integer(player_index) do
     cur_path_char_index(game, player_index)
     |> Map.get(:char_index)
   end
 
-  def map_angle(cur_char_rotation), do: -1 * cur_char_rotation
+  @spec course_rotation(Game.t(), list(ViewChar.t()), integer()) :: float()
+  def course_rotation(%Game{} = game, view_chars, player_index)
+    when is_list(view_chars) and is_integer(player_index),
+    do: -1 * (cur_view_char(game, view_chars, player_index) |> Map.get(:rotation))
 
+  @spec text_path_extra_attrs(Game.t(), integer()) :: binary()
   def text_path_extra_attrs(%Game{course: %{paths: paths}}, path_index)
       when is_integer(path_index) do
     paths
@@ -60,6 +73,7 @@ defmodule TypoKartWeb.RaceView do
     |> Enum.join(" ")
   end
 
+  @spec path_class(Course.t(), integer()) :: binary()
   def path_class(%Course{paths: paths}, path_index) when is_integer(path_index) do
     classes = "course path-#{path_index}"
     with %Path{extra_attrs: extra_attrs} <- Enum.at(paths, path_index),
@@ -73,8 +87,19 @@ defmodule TypoKartWeb.RaceView do
 
   defdelegate text_segments(game, path_index, player_index), to: GameMaster
 
-  defp cur_path_char_index(%Game{} = game, player_index) when is_integer(player_index) do
-    %Player{cur_path_char_indices: cur_path_char_indices} = Enum.at(game.players, player_index)
+  @spec cur_path_char_index(Game.t(), integer()) :: PathCharIndex.t()
+  def cur_path_char_index(%Game{players: players}, player_index) when is_integer(player_index) do
+    %Player{cur_path_char_indices: cur_path_char_indices} = Enum.at(players, player_index)
     hd(cur_path_char_indices)
+  end
+
+  @spec cur_view_char(Game.t(), list(ViewChar.t()), integer()) :: ViewChar.t()
+  def cur_view_char(%Game{} = game, view_chars, player_index) when is_list(view_chars) and is_integer(player_index) do
+    %{path_index: path_index, char_index: char_index} =
+      cur_path_char_index(game, player_index)
+
+    view_chars
+    |> Enum.at(path_index)
+    |> Enum.at(char_index)
   end
 end
