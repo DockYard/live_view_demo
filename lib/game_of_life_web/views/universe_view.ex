248abcd6ef4c5d26570956516510d9e0cdabab3d
kevlarr <echo1@dvnl.net>
2019-09-30 11:52:25 -0400

    Flashing gradient on party

--- lib/game_of_life_web/views/universe_view.ex
+++ lib/game_of_life_web/views/universe_view.ex
@@ -8,7 +8,7 @@ defmodule GameOfLifeWeb.UniverseView do
   alias GameOfLife.Cell.Position
 
   def render_universe(%Universe{generation: generation, dimensions: %Dimensions{width: width, height: height} = dimensions}, opts) do
-    content_tag(:section, class: universe_class(dimensions)) do
+    content_tag(:section, class: universe_class(dimensions, opts)) do
       Enum.map(0..(height - 1), fn y ->
         content_tag(:div, class: "cell-row") do
           Enum.map(0..(width - 1), fn x ->
@@ -29,9 +29,19 @@ defmodule GameOfLifeWeb.UniverseView do
 
   def template_names(), do: Template.names()
 
-  defp universe_class(%Dimensions{width: width, height: height}) when width > 200 or height > 200, do: "universe xxl"
-  defp universe_class(%Dimensions{width: width, height: height}) when width > 125 or height > 125, do: "universe xl"
-  defp universe_class(%Dimensions{width: width, height: height}) when width > 75 or height > 75, do: "universe lg"
-  defp universe_class(%Dimensions{width: width, height: height}) when width < 15 or height < 15, do: "universe sm"
-  defp universe_class(_dimensions), do: "universe md"
+  defp universe_class(dimensions, %{party: party, playing: playing}) do
+    "universe #{dimension_class(dimensions)}" |> playing?(playing) |> party?(party)
+  end
+
+  defp dimension_class(%Dimensions{width: width, height: height}) when width > 200 or height > 200, do: "xxl"
+  defp dimension_class(%Dimensions{width: width, height: height}) when width > 125 or height > 125, do: "xl"
+  defp dimension_class(%Dimensions{width: width, height: height}) when width > 75 or height > 75, do: "lg"
+  defp dimension_class(%Dimensions{width: width, height: height}) when width < 15 or height < 15, do: "sm"
+  defp dimension_class(_dimensions), do: "md"
+
+  defp playing?(str, true), do: "#{str} playing"
+  defp playing?(str, _), do: str
+
+  defp party?(str, true), do: "#{str} party"
+  defp party?(str, _), do: str
 end
